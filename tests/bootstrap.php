38deae0854e3ce88f9248d0c91f74d1c2c282aac
newLoki <newloki91@googlemail.com>
2012-05-18 20:47:14 +0200

    fix autoloading for unittests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,11 @@
 //require_once __DIR__.'/../vendor/.composer/autoload.php';
 require_once __DIR__.'/../app/silex.phar';
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
 
-$loader = new UniversalClassLoader();
-$loader = require_once __DIR__.'/../app/bootstrap.php';
-$loader->registerNamespace('Pollex\\Tests', __DIR__);
-$loader->register();
 
-return $loader;
+$app = require_once __DIR__.'/../app/app.php';
+//seems to be broken O.o
+$app['loader']->registerNamespace('Pollex\\Tests', __DIR__);
+
+
+return $app;
