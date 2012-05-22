e96148aaca6ffed017b3bda0ccedd5f01d9f489f
newLoki <newloki91@googlemail.com>
2012-05-22 12:24:31 +0200

    add doctrine2 orm service

--- app/app.php
+++ app/app.php
@@ -8,14 +8,23 @@ require_once __DIR__.'/bootstrap.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
-/**
- * Register own namepsace
- */
-$app['autoloader']->registerNamespace('SilexExtension', __DIR__ . '/../vendor/silex-extension/src');
-
 //register config dir
 $app->register(new KevinGH\Silex\Config\Provider, array(
     'config.path' => __DIR__ . '/config'
+));
+
+//register Doctrine ORM extension
+$app['autoloader']->registerNamespace('Nutwerk', __DIR__ . '/vendor/nutwerk-orm-extension/lib');
+$app->register(new Nutwerk\Provider\DoctrineORMServiceProvider(), array(
+    'db.orm.class_path'            => __DIR__.'/../vendor/doctrine2-orm/lib',
+    'db.orm.proxies_dir'           => __DIR__.'/../var/cache/doctrine/Proxy',
+    'db.orm.proxies_namespace'     => 'DoctrineProxy',
+    'db.orm.auto_generate_proxies' => true,
+    'db.orm.entities'              => array(array(
+        'type'      => 'annotation',
+        'path'      => __DIR__.'/../src/entites',
+        'namespace' => 'Pollex\Entity',
+    )),
 ));
 
 //ensure that content type is json
@@ -26,39 +35,29 @@ $app->before(function ($request) {
     }
 });
 
+//ensure it is authentificated
+//look @ http://chemicaloliver.net/programming/http-basic-auth-in-silex/
+$app->before(function() use ($app)
+{
+    if (!isset($_SERVER['PHP_AUTH_USER'])) {
+        $domain = $app['config']->get('domain');
+        header('WWW-Authenticate: Basic realm="' . $domain . '"');
+        return $app->json(array('Message' => 'Not Authorised'), 401);
+    } else {
+        //get user given in $_SERVER['PHP_AUTH_USER'] and check if
+        //password is same as $_SERVER['PHP_AUTH_PW']
+        //if not -> return $app->json(array('Message' => 'Forbidden'), 403);
+
+
+    }
+});
+
 $app->get('/', function() use ($app) {
 
     $test = new stdClass();
     $test->name = 'foo';
 
     return $app->json($test);
-});
-
-//ensure it is authentificated
-//look @ http://chemicaloliver.net/programming/http-basic-auth-in-silex/
-$app->before(function() use ($app)
-{
-    if (!isset($_SERVER['PHP_AUTH_USER']))
-    {
-        $domain = $app['config']->get('domain');
-        header('WWW-Authenticate: Basic realm="' . $domain . '"');
-        return $app->json(array('Message' => 'Not Authorised'), 401);
-    }
-    else
-    {
-        //once the user has provided some details, check them
-        $users = array(
-            'workflow' => 'password'
-            );
-
-        if($users[$_SERVER['PHP_AUTH_USER']] !== $_SERVER['PHP_AUTH_PW'])
-        {
-            //If the password for this user is not correct then resond as such
-            return $app->json(array('Message' => 'Forbidden'), 403);
-        }
-
-        //If everything is fine then the application will carry on as normal
-    }
 });
 
 return $app;
