3973a3e93d7f7f4be73aeff18b4081593140fa4a
newLoki <newloki91@googlemail.com>
2012-05-22 12:23:36 +0200

    add user entity

---
+++ src/entities/User.php
@@ -0,0 +1,229 @@
+<?php
+namespace Pollex\Entity;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @Entity @Table(name="user")
+ * @HasLifecycleCallbacks
+ */
+class User
+{
+    /**
+     * @Id @Column(type="integer") @GeneratedValue
+     * @var int
+     **/
+    protected $id;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     **/
+    protected $surname;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $lastname;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @Column(type="datetime")
+     * @var \DateTime
+     */
+    protected $birthdate;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * @Column(type="datetime")
+     * @var \DateTime
+     */
+    protected $created;
+
+    /**
+     * @Column(type="datetime")
+     * @var \DateTime
+     */
+    protected $updated;
+
+    /**
+     * Set created date to now, if a new object is constructed
+     * (this is not called on hydration)
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->created = new \DateTime();
+    }
+
+    /**
+     * Set updated field to now()
+     * @PrePersist
+     */
+    public function createUpdateDateTime()
+    {
+        $this->updated = new \DateTime();
+    }
+
+    /**
+     * set birthdate for user
+     *
+     * @param \DateTime $birthdate
+     */
+    public function setBirthdate($birthdate)
+    {
+        $this->birthdate = $birthdate;
+    }
+
+    /**
+     * get the birthdate for this user
+     *
+     * @return \DateTime
+     */
+    public function getBirthdate()
+    {
+        return $this->birthdate;
+    }
+
+    /**
+     * set datetime, when entry was created
+     *
+     * @param \DateTime $created
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * Return, when user was created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set users email address
+     *
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get users email address
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+
+    /**
+     * Get user id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set lastname for user
+     *
+     * @param string $lastname
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * Get lastname of user
+     *
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * set password for user
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Get user password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Setter for surname
+     *
+     * @param string $surname
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * Getter for surname
+     *
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * setter, when user was last updated
+     *
+     * @param \DateTime $updated
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    }
+
+    /**
+     * Get when user was last updated
+     *
+     * @return \DateTime
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+}
