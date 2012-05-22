700510f6d4d64a6cb35f20e5c15e299dc4c9a87a
newLoki <newloki91@googlemail.com>
2012-05-22 13:21:36 +0200

    add tests for user entity

--- README.markdown
+++ README.markdown
@@ -12,14 +12,15 @@ The full documentation, wrapped in a nice design, is availeaable under (http://n
 _Pollex_ is a huge fan of composer, so you should use it to manage dependencies.
 This means you have to type `php composer.phar install` to install all needed packages.
 
-##Setup the database
-Create a new database called pollex by typing
-`mysql -u<username> -p<password> -e "CREATE DATABASE IF NOT EXISTS pollex"`
-If the database is successfull created, you can bring your database schema to the max by typing:
-`java -jar liquibase.jar --driver=com.mysql.jdbc.Driver \
-      --classpath=databasedriver/mysql-connector-java-5.1.17-bin.jar \
-      --changeLogFile=../data/sql/changelog.xml \
-      --url="jdbc:mysql://127.0.0.1:3306/pollex" \
-      --username=root \
-      --password=root \
-      migrate`
+##Snippet for using Doctrine2 with Silex
+
+###Working with models
+`$article = new MyWebsite\Entity\Article();
+ $article->setContent('Hello world!');
+ $app['db.orm.em']->persist($article);
+ $app['db.orm.em']->flush();`
+[Source](http://martinsikora.com/silex-doctrine2-orm)
+
+###Working with custom queries
+`$app['db.orm.em']->createQuery('SELECT a FROM MyWebsite\Entity\Article a');`
+[Source](http://martinsikora.com/silex-doctrine2-orm)
---
+++ tests/Pollex/Entity/UserTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Pollex\Tests;
+
+use Pollex\Entity as Entity;
+
+class UserTest extends TestCase
+{
+    /** @var \Pollex\Entity\User */
+    protected $_user;
+
+    public function setUp()
+    {
+        $this->_user = new \Entity\User();
+    }
+
+    public function testShouldInstanciaeteGroups()
+    {
+        $this->assertInstanceOf(
+            'Doctrine\Common\Collections\ArrayCollection',
+            $this->_user->getGroups()
+        );
+    }
+
+    public function testCreatedTime()
+    {
+        $this->assertInstanceOf(
+            'DateTime',
+            $this->_user->getCreated()
+        );
+    }
+
+    public function testUpdatesTime()
+    {
+        $this->_user->createUpdateDateTime();
+        $this->assertInstanceOf(
+            'DateTime',
+            $this->_user->getUpdated()
+        );
+    }
+
+    public function testBirthdate()
+    {
+        $date = new \DateTime();
+        $this->_user->setBirthdate($date);
+        $this->assertInstanceOf(
+            'DateTime',
+            $this->_user->getBirthdate()
+        );
+        $this->assertEquals($date, $this->_user->getBirthdate());
+
+    }
+
+    public function testEmail()
+    {
+        $email = 'foo@bar.de';
+        $this->_user->setEmail($email);
+        $this->assertEquals($email, $this->_user->getEmail());
+    }
+
+    public function testLastname()
+    {
+        $lastname = 'doe';
+        $this->_user->setLastname($lastname);
+        $this->assertEquals($lastname, $this->_user->getLastname());
+    }
+
+    public function testSurname()
+    {
+        $surname = 'john';
+        $this->_user->setSurname($surname);
+        $this->assertEquals($surname, $this->_user->getSurname());
+    }
+
+    public function testPassword()
+    {
+        $password = md5('foobar');
+        $this->_user->setPassword($password);
+        $this->assertEquals($password, $this->_user->getPassword());
+    }
+
+    //@todo tests for groups
+}
