b7ea33ad9c32cb96512d78914be91dc731156130
newLoki <newloki91@googlemail.com>
2012-06-09 11:24:02 +0200

    add configuration for travis

--- test.php
+++ test.php
@@ -2,5 +2,7 @@
 
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'citest'));
+$env = APPLICATION_ENV;
+$config = require __DIR__ . '/app/config.php';
 
-echo APPLICATION_ENV;
+echo $config['db'][$env];
