4be72a24e67c1da1c7d8a1437e6a09f8316972f0
newLoki <newloki91@googlemail.com>
2012-06-19 07:22:28 +0200

    add incomplete url parts test

--- tests/Tests/Entity/BaseTest.php
+++ tests/Tests/Entity/BaseTest.php
@@ -48,10 +48,33 @@ class BaseTest extends \Tests\TestCase
         $this->_entity->setId(1);
         $this->assertEquals('/fakes/1', $this->_entity->getUrl());
     }
+
+    public function testGetUrlParts()
+    {
+        $this->_entity->setId(1);
+        $this->assertEquals(array('fakes', 1), $this->_entity->getUrlParts());
+    }
+
+    public function testGetDeepUrlParts()
+    {
+        $this->markTestIncomplete();
+        $poll = new Entity\Poll();
+        $poll->setId(1);
+        $question = new Entity\Poll\Question();
+        $question->setId(2);
+        $poll->addQuestion($question);
+        $urlParts = $question->getUrlParts();
+        $expected = array(
+            'polls',
+            1,
+            'questions',
+            2
+        );
+
+        $this->assertEquals($expected, $urlParts);
+    }
 }
 
 /** This exists only to test abstract class */
 class Fake extends \Pollex\Entity\Base {
-    CONST TYPE = 'fake';
-
 }
