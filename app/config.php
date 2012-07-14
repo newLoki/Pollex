5918c3c8d737647722ebdcca0279985d195a0054
newLoki <newloki91@googlemail.com>
2012-07-14 17:34:47 +0200

    add config for dotcloud production environment

--- app/config.php
+++ app/config.php
@@ -1,16 +1,23 @@
 <?php
 
+function tranformsDotCloud()
+{
+    $cloudConfig = json_decode(file_get_contents("/home/dotcloud/environment.json"),true);
+
+    return array(
+        'driver' => 'pdo_mysql',
+        'dbname' => 'pollex',
+        'host' => $cloudConfig['DOTCLOUD_DB_MYSQL_HOST'],
+        'user' => $cloudConfig['DOTCLOUD_DATA_MYSQL_LOGIN'],
+        'password' => $cloudConfig['DOTCLOUD_DATA_MYSQL_PASSWORD'],
+        'port' => $cloudConfig['DOTCLOUD_DATA_MYSQL_PORT'],
+    );
+}
+
 return array(
     // database settings
     'db' => array(
-        'prod' => array(
-            'driver'    => 'pdo_mysql',
-            'dbname'    => 'pollex',
-            'host'      => '127.0.0.1',
-            'user'      => 'root',
-            'password'  => 'root',
-            'port'      => '3306'
-        ),
+        'prod' => tranformsDotCloud(),
         'dev' => array(
             'driver'    => 'pdo_mysql',
             'dbname'    => 'pollex',
