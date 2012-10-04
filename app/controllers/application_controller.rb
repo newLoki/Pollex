14a5300c25ccd2512568ebbba7efc61f9afbd1a3
newLoki <newloki91@googlemail.com>
2012-10-03 20:12:49 -0700

    remove my own responder, this is a crappy one, which is not autoloaded and since iam not a railler, i drop it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,4 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  #add default responder to responde with correct headers on update/creation
-  self.responder = ApiResponder
 end
