d0a928fcc2c3989c680117920be6b5bec5c964e4
newLoki <newloki91@googlemail.com>
2012-10-03 17:34:07 -0700

    initial rails api commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollex::Application.config.secret_token = '241d7f1934e0bb325a56479e6d79e9bc8edf3206977cc4b0d244505150362956190701d6ce2da54c70eff767352aa9ac77d746fad20ca1accdb4e8bcd4183c20'
