87c7e0ec1d9f52241cc7b19a0cadd35e8a188234
newLoki <newloki91@googlemail.com>
2012-10-03 19:18:27 -0700

    add create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,27 @@ class UsersController < ApplicationController
   #ensure that it can return json and xml
   respond_to :json, :xml
 
+  #method to list all users
+  def index
+    @users = User.all
+  end
+
   #method to find a user by given id
   def show
     @user = User.find_by_id(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save?
+        format.xml   { render :xml => @user, :status => :created, :location => @user }
+        format.json  { render :json => @user, :status => :created, :location => @user }
+      else
+        format.xml { render :xml => @user.errors, :status => :unprocessable_entity }
+        format.json { render :json => @user.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 end
