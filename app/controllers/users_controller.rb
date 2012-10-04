90dba9e7ffc3da5db32c3ccf23562d703d723971
newLoki <newloki91@googlemail.com>
2012-10-03 18:09:57 -0700

    add user controller and view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  #require oauth login before any action is performed
+  before_filter :oauth_required
+
+  #ensure that it can return json and xml
+  respond_to :json, :xml
+
+  #method to find a user by given id
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+end
