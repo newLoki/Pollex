f0c58c96828deee390e05486b9678e8a8a2c5f91
newLoki <newloki91@googlemail.com>
2012-10-04 17:51:23 -0700

    implement delete, update an create actions for questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
   def index
     @poll = Poll.find_by_id(params[:poll_id])
     if @poll.nil?
-      @errors = { :messages => "No poll with id #{params[:id]} found" }
+      @errors = { :messages => "No poll with id #{params[:poll_id]} found" }
       render :error, :status => 404, :formats => [:json]
     else
       @questions = @poll.questions
@@ -12,7 +12,7 @@ class QuestionsController < ApplicationController
   def show
     @poll = Poll.find_by_id(params[:poll_id])
     if @poll.nil?
-      @errors = { :messages => "No poll with id #{params[:id]} found" }
+      @errors = { :messages => "No poll with id #{params[:poll_id]} found" }
       render :error, :status => 404, :formats => [:json]
     else
       @question = @poll.questions.find_by_id(params[:id])
@@ -21,6 +21,64 @@ class QuestionsController < ApplicationController
         @errors = { :messages => "No question with id #{params[:id]} found" }
         render :error, :status => 404, :formats => [:json]
       end
+    end
+  end
+
+  def create
+    @poll = Poll.find_by_id(params[:poll_id])
+    if @poll.nil?
+      @errors = { :messages => "No poll with id #{params[:poll_id]} found" }
+      render :error, :status => 404, :formats => [:json]
+    else
+      @question = Question.new(JSON.parse(params[:question]))
+      @question.poll = @poll
+
+      if @question.valid?
+        @question.save
+        render :update, :status => :ok, :formats => [:json]
+      else
+        @errors = @question.errors
+        render :error, :status => :conflict, :formats => [:json]
+      end
+    end
+  end
+
+  def update
+    @poll = Poll.find_by_id(params[:poll_id])
+    if @poll.nil?
+      @errors = { :messages => "No poll with id #{params[:poll_id]} found" }
+      render :error, :status => 404, :formats => [:json]
+    else
+
+      @question = Question.find_by_id(params[:id])
+
+      if @question.nil?
+        @errors = { :messages => "No question with id #{params[:id]} found" }
+        render :error, :status => 404, :formats => [:json]
+      else
+        @question.update_attributes(JSON.parse(params[:question]))
+        @question.poll = @poll
+
+        if @question.valid?
+          @question.save
+          render :update, :status => :ok, :formats => [:json]
+        else
+          @errors = @question.errors
+          render :error, :status => :conflict, :formats => [:json]
+        end
+      end
+    end
+  end
+
+  def destroy
+    @question = Question.find_by_id(params[:id])
+
+    if @question.nil?
+      @errors = { :messages => "No question with id #{params[:id]} found" }
+      render :error, :status => 404, :formats => [:json]
+    else
+      @question.destroy
+      render :destroy, :status => :ok, :formats => [:json]
     end
   end
 end
---
+++ app/views/questions/destroy.rabl
@@ -0,0 +1,3 @@
+object @question
+
+attribute :id
