1311002a85c3d294dd18461d536944d48a375fd4
newLoki <newloki91@googlemail.com>
2012-10-04 18:57:08 -0700

    add validations for value, relation for user and question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,16 @@
 class Answer < ActiveRecord::Base
-  attr_accessible :poll_id, :question_id, :type_id, :value
-  belongs_to :questions
-  belongs_to :polls
-  belongs_to :users
-  belongs_to :groups
+  attr_accessible :question_id, :type_id, :value
+  belongs_to :question
+  belongs_to :user
+  belongs_to :group
+  #belongs_to :type
+
+  validates :value, :presence => true,
+            :format => {
+                :with => /\A[a-z0-9\.\?! -_]+\z/i,
+                :message => "Only letters, numbers, ?!-_. and whitespaces allowed"
+            }
+  validates_presence_of :user
+  validates_presence_of :question
+  #validates_presence_of :type
 end
