6dee7f1f583e5f8da21b027db2f2fbd9e1b319a5
newLoki <newloki91@googlemail.com>
2012-10-04 19:04:09 -0700

    add create action for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -37,4 +37,29 @@ class AnswersController < ApplicationController
       end
     end
   end
+
+  def create
+    @poll = Poll.find_by_id(params[:poll_id])
+    if @poll.nil?
+      @errors = { :messages => "No poll with id #{params[:poll_id]} found" }
+      render :error, :status => 404, :formats => [:json]
+    else
+      @question = @poll.questions.find_by_id(params[:question_id])
+
+      if @question.nil?
+        @errors = { :messages => "No question with id #{params[:question_id]} found" }
+        render :error, :status => 404, :formats => [:json]
+      else
+        @answer = Answer.new(JSON.parse(params[:answer]))
+
+        if @answer.valid?
+          @answer.save
+          render :update, :status => :ok, :formats => [:json]
+        else
+          @errors = @answer.errors
+          render :error, :status => :conflict, :formats => [:json]
+        end
+      end
+    end
+  end
 end
